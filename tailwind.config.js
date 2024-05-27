64c88e34b22a6e02d464fc1f9c3629bbe697c328
Maria Jose <mariabecerrab17@gmail.com>
2024-05-27 17:46:24 -0500

    feat: init repository

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        bali: "url('../img/bali.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+        LA: "url('../img/LA.jpg')",
+        miami: "url('../img/miami.jpg')",
+        newYork: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9",
+      }),
+      textColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    width: ["responsive", "hover", "focus"],
+    extend: {},
+  },
+  plugins: [],
+};
